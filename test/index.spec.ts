5d2d560be63cf5c6fd256c3736d2f4d8e826888f
Daniele Vitale <alu0101329017@ull.edu.es>
2022-05-08 21:12:34 +0000

    First commit

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,20 +1,9 @@
 import 'mocha';
 import {expect} from 'chai';
-import {EventEmitter} from 'events';
-import {MessageEventEmitterClient} from '../src/eventEmitterClient';
+import {add} from '../src/index';
 
-describe('MessageEventEmitterClient', () => {
-  it('Should emit a message event once it gets a complete message', (done) => {
-    const socket = new EventEmitter();
-    const client = new MessageEventEmitterClient(socket);
-
-    client.on('message', (message) => {
-      expect(message).to.be.eql({'type': 'change', 'prev': 13, 'curr': 26});
-      done();
-    });
-
-    socket.emit('data', '{"type": "change", "prev": ');
-    socket.emit('data', ', "curr": 26}');
-    socket.emit('data', '\n');
+describe('add', () => {
+  it('should add two numbers', () => {
+    expect(add(1, 2)).to.equal(3);
   });
 });
